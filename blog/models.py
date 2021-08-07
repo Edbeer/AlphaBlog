66baac2ce689fd60b2ebe22bbed3d071ba065ee9
Edbeer <edbeermtn@gmail.com>
2021-08-07 21:30:46 +0700

    add models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+"""
+Post: (title, slug, content, photo, created_at, views, is_published)
+Comment: (post, name, content, created_at)
+"""
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, verbose_name='URL', unique=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    content = models.TimeField(max_length=1500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
+
+    class Meta:
+        ordering = ['-created_at']
