2b27105e6384c044fc64f3f8ab36c55e02f0a59a
Edbeer <edbeermtn@gmail.com>
2021-08-07 21:53:16 +0700

    django-debug-toolbar

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 
 
